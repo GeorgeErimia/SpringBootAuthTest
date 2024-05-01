b292412995dde72cf728ebe9eb6595f6a5288118
George Erimia <l3ttuceftw@gmil.com>
2024-05-01 20:34:42 +0300

    Initial Commit (Added Project Files)

---
+++ src/main/java/com/georgeerimia/SpringBootAuthTest/SpringBootAuthTestApplication.java
@@ -0,0 +1,13 @@
+package com.georgeerimia.SpringBootAuthTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootAuthTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAuthTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/georgeerimia/SpringBootAuthTest/SpringBootAuthTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.georgeerimia.SpringBootAuthTest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootAuthTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
